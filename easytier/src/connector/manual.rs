ef000c7d5cd83f5cdc94f831eb805717183484b4
cn1095 <1095053105@qq.com>
2025-02-15 15:10:03 +0800

    Update manual.rs

--- easytier/src/connector/manual.rs
+++ easytier/src/connector/manual.rs
@@ -307,7 +307,6 @@ impl ManualConnectorManager {
         )
         .await;
     }
-   use url::Url;
 
 let remote_url = match Url::parse(&dead_url) {
     Ok(url) => url, 
@@ -318,7 +317,7 @@ let remote_url = match Url::parse(&dead_url) {
 };
 
 println!("🔍 实际连接的服务器: {}", remote_url);
-
+connector.lock().await.set_remote_url(remote_url.clone());
 data.global_ctx.issue_event(GlobalCtxEvent::Connecting(remote_url.clone()));
 println!("📡 连接事件已发送: {}", remote_url);
 
