7668fa837c830e5b0d59c4a2b6131b0299c91ff1
cn1095 <1095053105@qq.com>
2025-03-04 11:31:57 +0800

    Update config_generator.ts

--- easytier-web/frontend/src/config_generator.ts
+++ easytier-web/frontend/src/config_generator.ts
@@ -1,16 +1,10 @@
 import { createApp } from 'vue';
 import ConfigGenerator from './components/ConfigGenerator.vue';
 import PrimeVue from 'primevue/config';
-import ToastService from 'primevue/toastservice';
-import DialogService from 'primevue/dialogservice';
-import ConfirmationService from 'primevue/confirmationservice';
 import EasytierFrontendLib from 'easytier-frontend-lib';
 
 const app = createApp(ConfigGenerator);
 
 app.use(PrimeVue)
-   .use(ToastService as any)
-   .use(DialogService as any)
-   .use(ConfirmationService as any)
    .use(EasytierFrontendLib)
    .mount('#app');
