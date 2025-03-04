f54778eaf322c1cc91562fffc21e637a3023a6a1
cn1095 <1095053105@qq.com>
2025-03-04 11:16:31 +0800

    Update vite.config.ts

--- easytier-web/frontend/vite.config.ts
+++ easytier-web/frontend/vite.config.ts
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { viteSingleFile } from "vite-plugin-singlefile"
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '',
-  plugins: [vue(), viteSingleFile()],
-})
+    plugins: [vue()],
+    build: {
+        outDir: 'dist',
+        rollupOptions: {
+            input: 'index.html'  // 让 Vite 以 index.html 为入口
+        }
+    }
+});
