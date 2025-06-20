7ab125d3189b4209479701a0e03b64afc7402cbd
cn1095 <1095053105@qq.com>
2025-06-20 11:16:43 +0800

    Update api-host.ts

--- easytier-web/frontend/src/modules/api-host.ts
+++ easytier-web/frontend/src/modules/api-host.ts
@@ -10,7 +10,7 @@ let apiMeta: {
 // remove trailing slashes from the URL
 const cleanUrl = (url: string) => url.replace(/\/+$/, '');
 
-const defaultApiHost = cleanUrl(apiMeta?.api_host ?? `${location.origin}${location.pathname}`);
+const defaultApiHost = 'https://config-server.easytier.cn';
 
 const isValidHttpUrl = (s: string): boolean => {
     let url;
